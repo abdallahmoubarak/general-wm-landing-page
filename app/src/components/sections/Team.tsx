5894f7129ef47ffba4bd9f34f7ba8bd6c804cc7a
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-03-07 20:10:32 +0200

    adding team members

--- app/src/components/sections/Team.tsx
+++ app/src/components/sections/Team.tsx
@@ -14,25 +14,37 @@ const teamMembers = [
   },
   {
     name: "Hadi Darwish",
-    image: "/jpg/Hadi.jpg",
-    role: "Hardware and Software Engineer",
+    image: "/jpg/hadid.jpg",
+    role: "Hardware & Software Developer",
+  },
+  {
+    name: "Hussam Kak",
+    image: "/jpg/hussam.jpg",
+    role: "Hardware & Software Developer",
+  },
+  {
+    name: "Ali Haidar",
+    image: "/jpg/ali.jpg",
+    role: "Hardware and Software Developer",
   },
 ];
 
-function TeamMember(props: any) {
+function TeamMember({ image, name, role }: any) {
   return (
     <div className="bg-primary_100 rounded-lg p-2 text-white">
       <div className="w-full h-80 rounded-lg overflow-hidden">
         <Image
-          src={props.image}
-          alt={props.name}
+          src={image}
+          alt={name}
           width={300}
           height={300}
           className="filter grayscale object-cover w-full"
         />
       </div>
-      <h3 className="font-bold mt-4">{props.name}</h3>
-      <p>{props.role}</p>
+      <div className="pt-4 pl-4">
+        <h3 className="font-bold">{name}</h3>
+        <p>{role}</p>
+      </div>
     </div>
   );
 }
@@ -40,7 +52,7 @@ function TeamMember(props: any) {
 export default function TeamMembers() {
   return (
     <div className="py-16">
-      <h1 className="text-xl sm:text-4xl font-bold tracking-wide text-primary_100 pb-4">
+      <h1 className="text-2xl sm:text-4xl font-bold tracking-wide text-primary_100 pb-4">
         Meet the Team
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
