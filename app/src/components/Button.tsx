fd3e81bbe3d22260756a8b9822ad3bfaf4b809ae
Yasser Shkeir <shkeiryasser@gmail.com>
2023-03-09 18:57:20 +0200

    Components: Button-> Improve Readability for Desktop

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export default function Button({ text = "button", icon }: buttonProps) {
   return (
-    <button className="flex gap-2 items-center bg-primary_100 text-white py-2 px-4 rounded-full hover:bg-primary_80 hover:text-black_gray whitespace-nowrap">
-      <span>{icon}</span>
+    <button className="flex items-center justify-center bg-primary_100 text-white text-sm rounded-full py-3 px-4 hover:bg-primary_80 hover:text-black_gray transition-all duration-200 ease-in-out md:min-w-[240px] md:text-xl md:py-4 md:px-8">
+      <span className="mr-2">{icon}</span>
       <span>{text}</span>
     </button>
   );
