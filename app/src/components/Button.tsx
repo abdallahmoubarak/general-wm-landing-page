e5ce516f0a0012d5375ecfcfb6c364688adc6ee4
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-03-09 19:19:07 +0200

    adding transition

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Button({ text = "button", icon }: buttonProps) {
   return (
-    <button className="flex gap-2 items-center bg-primary_100 text-white py-2 px-4 rounded-full hover:bg-primary_80 hover:text-black_gray whitespace-nowrap">
+    <button className="flex gap-2 items-center bg-primary_100 text-white py-2 px-4 rounded-full hover:bg-primary_80 hover:text-black_gray whitespace-nowrap transition-all duration-200 ease-in-out">
       <span>{icon}</span>
       <span>{text}</span>
     </button>
