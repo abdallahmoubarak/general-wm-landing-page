2664a914733b724f5606373e10060f3b5b16541b
Yasser Shkeir <shkeiryasser@gmail.com>
2023-04-03 16:46:23 +0300

    Components: Sections-> Details Improve Language

--- app/src/components/sections/Details.tsx
+++ app/src/components/sections/Details.tsx
@@ -27,20 +27,20 @@ export default function Details() {
 
 const sections = [
   {
-    title: "Track Water Usage & Plan Your House Chores",
+    title: "Track Water Usage & Plan Your Chores Efficiently",
     img: "/png/track.png",
-    text: "Track the amount of water being used and receive notifications when the water levels in your tanks is low, allowing you to plan your house chores accordingly.",
+    text: "Keep a close eye on your water usage and receive timely notifications when water levels in your tanks run low. With this information, you can plan your chores more efficiently and save water.",
     left: true,
   },
   {
-    title: "Prevent Water Shortages, by Planning Ahead",
+    title: "Prevent Water Shortages on a Large Scale",
     img: "/png/waterlack.png",
-    text: "We assist NGOs by implementing our Wide Network Tracking Plans in Refugee camps, giving NGOs enough time to plan ahead, and prevent water shortages.",
+    text: "We collaborate with governments, municipalities, and NGOs to implement our wide network tracking plans, which provide valuable insights and ample time to plan ahead, and prevent water shortages.",
     left: false,
   },
   {
-    title: "Detect Water Leaks, With the Power of AI",
-    text: "Our AI is able to detect even the slightest of leaks, by analyzing your usage patterns,ensuring that you can take action to fix the problem before it becomes a bigger issue.",
+    title: "Detect Water Leaks Before It's Too Late",
+    text: "Our advanced AI technology can detect even the slightest of leaks by analyzing your usage patterns. You'll receive instant alerts, so you can take prompt action to fix the problem before it becomes a bigger issue.",
     img: "/png/alert.png",
     left: true,
   },
